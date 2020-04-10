8bc1f621d58e3ca94775c124e9ba2110e10dc4b8
Carlos Alejandro Avilez Jimenez <carlos.avilez@softtek.com>
2020-04-10 12:36:46 -0700

    Updates

---
+++ precache-manifest.4a3cd0817d6bac8d8e9fb9e57deb190a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3faee099327c5d3e7d6b0aceb0efc9",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1fb4df9283f75b9e40c7",
+    "url": "/my-app/static/css/main.4603c898.chunk.css"
+  },
+  {
+    "revision": "55a30ad748f3b731b8ed",
+    "url": "/my-app/static/js/2.b3d719c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.b3d719c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb4df9283f75b9e40c7",
+    "url": "/my-app/static/js/main.51321fbd.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  }
+]);
